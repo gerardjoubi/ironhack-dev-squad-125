c81257e8b927e3ebd38a926c590ec74dc92bf7f6
owlab-io <contact@owlab.io>
2019-04-17 23:49:13 +0200

    renamed folder

--- 02_week_2/1_scope_oop/0_function_scope/module.js
+++
@@ -1,7 +0,0 @@
-console.log("-----------------------\n@module.js\n-----------------------");
-console.log("a globalVarIsEvil =>", window.globalVarIsEvil);
-console.log("b globalVarToo =>", window.globalVarToo);
-console.log("b wontBeCheck =>", window.wontBeCheck);
-console.log("c wontBeCheckedNeither =>", window.wontBeCheckedNeither);
-console.log("d =>", this);
-// previous statement: module scripts use 'strict mode' as default, so this will throw a type Error
