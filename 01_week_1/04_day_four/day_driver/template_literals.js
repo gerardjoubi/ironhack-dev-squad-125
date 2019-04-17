c5f8709e5ab1b9adb93d0ba12419618421b963b0
owlab-io <contact@owlab.io>
2019-04-17 23:48:17 +0200

    renamed folder

--- 01_week_1/04_day_four/day_driver/template_literals.js
+++
@@ -1,5 +0,0 @@
-// TEMPLATE LITERALS
-var username = "gui";
-console.log("hello " + username); // concatenate !
-console.log(`hello ${username}`); // interpolate ! check mdn: template literals
-console.log(`hello username`); // won't work since it's a simple string !
