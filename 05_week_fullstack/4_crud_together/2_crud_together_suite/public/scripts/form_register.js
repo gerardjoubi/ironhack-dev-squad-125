b8876f801a3ef3fcc6c1dbe934f22e9f773cf4ae
owlab-io <contact@owlab.io>
2019-04-25 14:36:09 +0200

    added frontend function triggering file popup @click on an icon

--- 05_week_fullstack/4_crud_together/2_crud_together_suite/public/scripts/form_register.js
+++ 05_week_fullstack/4_crud_together/2_crud_together_suite/public/scripts/form_register.js
@@ -1,7 +1,13 @@
-const icon = document.getElementById("reveal_password");
+const iconReveal = document.getElementById("reveal_password");
+const iconAvatar = document.getElementById("avatar");
+const inputFile = document.getElementById("file_input");
 const inputPassword = document.getElementById("password");
 
-icon.onclick = function reavealPassword() {
+iconAvatar.onclick = function displayFileBox() {
+  inputFile.click();
+};
+
+iconReveal.onclick = function reavealPassword() {
   icon.classList.toggle("fa-eye");
   icon.classList.toggle("fa-eye-slash");
   inputPassword.type = icon.classList.contains("fa-eye-slash")
