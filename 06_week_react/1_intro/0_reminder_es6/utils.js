556b0f22e76d6e1556b76ffe6b8da9c61d51e8ef
owlab-io <contact@owlab.io>
2019-04-29 18:48:02 +0200

    removed unused files after folder rename

--- 06_week_react/1_intro/0_reminder_es6/utils.js
+++
@@ -1,6 +0,0 @@
-export default function brandIsAdidas(b) {
-    // only one default export per file
-  return b === "Adidas";
-}
-
-export const countLetters = (word)  =>  word.length;
--- 06_week_react/1_intro/2_first_components/src/HelloReact/HelloReact.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import "./HelloReact.css";
-
-export default function helloReact() {
-    return <div className="hello">Hello React !</div>
-}
